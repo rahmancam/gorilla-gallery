a24ab495a325e2c7e1154989c38d5f9eced51456
Abdul Rahman <rahmancam@live.com>
2021-02-10 02:32:53 +0530

    Add Parsing form with gorilla schema

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/schema"
+)
 
 // User type
 type User struct{}
@@ -10,6 +15,24 @@ func NewUserController() *User {
 	return &User{}
 }
 
+// SignupForm type holds all user signup form submit data
+type SignupForm struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var decoder = schema.NewDecoder()
+
 // Create new user on signup
 func (u *User) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	var formData SignupForm
+	if err := decoder.Decode(&formData, r.PostForm); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(w, formData.Email, formData.Password)
 }
